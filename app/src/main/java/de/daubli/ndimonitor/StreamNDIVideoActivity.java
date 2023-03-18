77ca5ecc10f227dd36f6ffb53d0e78de35e2f1ff
daubli <daniel@daubli.de>
2023-03-18 16:29:41 +0100

    Get rid of double bitmap drawing
    + optimizations in render loop timing

--- app/src/main/java/de/daubli/ndimonitor/StreamNDIVideoActivity.java
+++ app/src/main/java/de/daubli/ndimonitor/StreamNDIVideoActivity.java
@@ -2,15 +2,11 @@ package de.daubli.ndimonitor;
 
 import java.nio.ByteBuffer;
 import java.util.Optional;
-import java.util.logging.Logger;
 
 import com.daubli.ndimonitor.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import android.media.AudioFormat;
-import android.media.AudioManager;
-import android.media.AudioRecord;
-import android.media.AudioTrack;
+import android.media.*;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
@@ -22,11 +18,9 @@ public class StreamNDIVideoActivity extends AppCompatActivity {
 
     DevolaySource ndiVideoSource;
     NdiVideoView ndiVideoView;
-    FloatingActionButton fab;
+    FloatingActionButton closeButton;
 
     boolean shouldInterrupt = false;
-    
-    private static final Logger LOG = Logger.getLogger(StreamNDIVideoActivity.class.getSimpleName());
 
     private static final int sampleRate = 48000;
     private static final int channelCount = 2;
@@ -39,103 +33,115 @@ public class StreamNDIVideoActivity extends AppCompatActivity {
         ndiVideoSource = MainActivity.getSource();
         setContentView(R.layout.stream_ndi_video_activity);
         ndiVideoView = findViewById(R.id.ndiVideoView);
-        fab = findViewById(R.id.regular_fab);
-        fab.setVisibility(View.VISIBLE);
+        closeButton = findViewById(R.id.regular_fab);
+        closeButton.setVisibility(View.VISIBLE);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        fab.setOnClickListener((View v) -> shouldInterrupt = true);
+        closeButton.setOnClickListener((View v) -> shouldInterrupt = true);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        new Thread(() -> {
-            try {
-                stream();
-            } catch (InterruptedException e) {
-                LOG.warning("Stream Interrupted: " + e.getMessage());
-                this.finish();
-            }
-        }).start();
+        new Thread(this::stream).start();
     }
 
-    private void stream() throws InterruptedException {
-        DevolayReceiver receiver = new DevolayReceiver();
-        receiver.connect(ndiVideoSource);
-        // Create initial frames to be used for capturing
-        DevolayVideoFrame videoFrame = new DevolayVideoFrame();
-        DevolayAudioFrame audioFrame = new DevolayAudioFrame();
+    private void stream() {
+        try (DevolayReceiver receiver = new DevolayReceiver()) {
+            receiver.connect(ndiVideoSource);
+            // Create initial frames to be used for capturing
+            DevolayVideoFrame videoFrame = new DevolayVideoFrame();
+            DevolayAudioFrame audioFrame = new DevolayAudioFrame();
 
-        // Setup frame to convert floating-point data to 16s data
-        DevolayAudioFrameInterleaved16s interleaved16s = new DevolayAudioFrameInterleaved16s();
-        interleaved16s.setReferenceLevel(20); // Recommended level for receiving in NDI docs
-        interleaved16s.setData(ByteBuffer.allocateDirect((int) (sampleRate / clockSpeed) * channelCount * Short.BYTES));
+            // Setup frame to convert floating-point data to 16s data
+            DevolayAudioFrameInterleaved16s interleaved16s = new DevolayAudioFrameInterleaved16s();
+            interleaved16s.setReferenceLevel(20); // Recommended level for receiving in NDI docs
+            interleaved16s.setData(
+                    ByteBuffer.allocateDirect((int) (sampleRate / clockSpeed) * channelCount * Short.BYTES));
 
-        final int minBufferSize = AudioRecord.getMinBufferSize(sampleRate,
-                AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT);
-        AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, sampleRate,
-                AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT, minBufferSize,
-                AudioTrack.MODE_STREAM);
+            final int minBufferSize = AudioRecord.getMinBufferSize(sampleRate,
+                    AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT);
+            AudioAttributes audioAttributes =
+                    new AudioAttributes.Builder().setLegacyStreamType(AudioManager.STREAM_MUSIC).build();
+            AudioFormat audioFormat =
+                    new AudioFormat.Builder().setSampleRate(sampleRate).setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                            .setChannelMask(AudioFormat.CHANNEL_OUT_STEREO).build();
+            AudioTrack audioTrack =
+                    new AudioTrack.Builder().setAudioAttributes(audioAttributes).setAudioFormat(audioFormat)
+                            .setBufferSizeInBytes(minBufferSize).setTransferMode(AudioTrack.MODE_STREAM).build();
 
-        DevolayFrameSync frameSync = new DevolayFrameSync(receiver);
-        // Attach the frame-synchronizer to ensure that audio is dynamically resampled based on request frequency.
-        try {
-            // Run for one minute
-            boolean isFirstFrame = true;
+            // Attach the frame-synchronizer to ensure that audio is dynamically resampled based on request frequency.
+            try (DevolayFrameSync frameSync = new DevolayFrameSync(receiver)) {
+                // Run for one minute
+                boolean isFirstFrame = true;
 
-            audioTrack.play();
+                audioTrack.play();
 
-            while (true) {
-                if (receiver.getConnectionCount() < 1) {
-                    receiver.connect(ndiVideoSource);
-                }
-
-                // Capture a video frame
-                if (frameSync.captureVideo(videoFrame)) { // Only returns true if a video frame was returned
-                    boolean finalIsFirstFrame = isFirstFrame;
-                    runOnUiThread(() -> Optional.ofNullable(videoFrame)
-                            .ifPresent(frame -> ndiVideoView.setCurrentFrame(videoFrame, finalIsFirstFrame)));
-
-                    if (isFirstFrame) {
-                        runOnUiThread(() -> ndiVideoView.setVisibility(View.VISIBLE));
+                while (true) {
+                    long startTimeStamp = System.currentTimeMillis();
+                    if (receiver.getConnectionCount() < 1) {
+                        receiver.connect(ndiVideoSource);
                     }
-                    isFirstFrame = false;
+
+                    // Capture audio samples
+                    frameSync.captureAudio(audioFrame, sampleRate, channelCount, (int) (sampleRate / clockSpeed));
+                    // Convert the given float data to interleaved 16s data
+                    DevolayUtilities.planarFloatToInterleaved16s(audioFrame, interleaved16s);
+                    // Get the audio data in a byte array, needed to write to a SourceDataLine
+                    int size = audioFrame.getSamples() * Short.BYTES * audioFrame.getChannels();
+                    byte[] audioData = new byte[size];
+                    interleaved16s.getData().get(audioData);
+
+                    audioTrack.write(audioData, 0, size);
+
+                    if (shouldInterrupt) {
+                        audioTrack.stop();
+                        throw new InterruptedException();
+                    }
+
+                    // Capture a video frame
+                    if (frameSync.captureVideo(videoFrame)) { // Only returns true if a video frame was returned
+                        runOnUiThread(() -> Optional.ofNullable(videoFrame)
+                                .ifPresent(frame -> ndiVideoView.setCurrentFrame(videoFrame)));
+
+                        if (isFirstFrame) {
+                            runOnUiThread(() -> ndiVideoView.setVisibility(View.VISIBLE));
+                        }
+                        isFirstFrame = false;
+                    }
+
+                    // Capture a video frame
+                    if (frameSync.captureVideo(videoFrame)) { // Only returns true if a video frame was returned
+                        runOnUiThread(() -> Optional.ofNullable(videoFrame)
+                                .ifPresent(frame -> ndiVideoView.setCurrentFrame(videoFrame)));
+
+                        if (isFirstFrame) {
+                            runOnUiThread(() -> ndiVideoView.setVisibility(View.VISIBLE));
+                            isFirstFrame = false;
+                        }
+                    }
+
+                    long loopDuration = System.currentTimeMillis() - startTimeStamp;
+
+                    if (loopDuration < (1000 / clockSpeed)) {
+                        Thread.sleep((long) (1000 / clockSpeed) - loopDuration);
+                    }
                 }
-
-                // Capture audio samples
-                frameSync.captureAudio(audioFrame, sampleRate, channelCount, (int) (sampleRate / clockSpeed));
-                // Convert the given float data to interleaved 16s data
-                DevolayUtilities.planarFloatToInterleaved16s(audioFrame, interleaved16s);
-                // Get the audio data in a byte array, needed to write to a SourceDataLine
-                int size = audioFrame.getSamples() * Short.BYTES * audioFrame.getChannels();
-                byte[] audioData = new byte[size];
-                interleaved16s.getData().get(audioData);
-
-                audioTrack.write(audioData, 0, size);
-
-                if (shouldInterrupt) {
-                    audioTrack.stop();
-                    break;
-                }
-                Thread.sleep((long) (1000 / clockSpeed));
+            } catch (InterruptedException interruptedException) {
+                videoFrame.close();
+                audioFrame.close();
+                runOnUiThread(() -> {
+                    ndiVideoView.setVisibility(View.INVISIBLE);
+                    ndiVideoView.deleteCurrentFrameData();
+                    // Make sure to close the framesync before the receiver
+                    this.finish();
+                });
+                Thread.currentThread().interrupt();
             }
-        } finally {
-            // Destroy the references to each. Not necessary, but can free up the memory faster than Java's GC by itself
-            videoFrame.close();
-            audioFrame.close();
-            runOnUiThread(() -> {
-                ndiVideoView.setVisibility(View.INVISIBLE);
-                ndiVideoView.deleteCurrentFrameData();
-                // Make sure to close the framesync before the receiver
-                frameSync.close();
-                receiver.close();
-
-                this.finish();
-            });
         }
     }
 }
--- app/src/main/java/de/daubli/ndimonitor/view/NdiVideoView.java
+++ app/src/main/java/de/daubli/ndimonitor/view/NdiVideoView.java
@@ -14,7 +14,6 @@ import me.walkerknapp.devolay.DevolayVideoFrame;
 
 public class NdiVideoView extends View {
 
-    private Bitmap currentFrameBitmapOld;
     private Bitmap currentFrameBitmap;
     private final Paint paint = new Paint(Paint.FILTER_BITMAP_FLAG);
     private int leftOffset = 0;
@@ -31,24 +30,15 @@ public class NdiVideoView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.save();
-
-        if (currentFrameBitmapOld != null) {
-            canvas.drawBitmap(currentFrameBitmapOld, leftOffset, 0, null);
-        }
-
-        if (currentFrameBitmap != null) {
-            canvas.drawBitmap(currentFrameBitmap, leftOffset, 0, paint);
-        }
-
+        canvas.drawBitmap(currentFrameBitmap, leftOffset, 0, paint);
         canvas.restore();
     }
 
     public void deleteCurrentFrameData() {
         this.currentFrameBitmap = null;
-        this.currentFrameBitmapOld = null;
     }
 
-    public void setCurrentFrame(DevolayVideoFrame videoFrame, boolean isFirstFrame) {
+    public void setCurrentFrame(DevolayVideoFrame videoFrame) {
         int heightOfView = this.getHeight();
         int widthOfView = this.getWidth();
 
@@ -67,9 +57,6 @@ public class NdiVideoView extends View {
         if (videoFrame.getFourCCType().equals(DevolayFrameFourCCType.BGRA)) {
             this.currentFrameBitmap =
                     BgraBitmapBuilder.builder().withFrame(videoFrame).withHeight(height).withWidth(width).build();
-        }
-        if (!isFirstFrame) {
-            this.currentFrameBitmapOld = currentFrameBitmap;
         }
 
         this.invalidate();
