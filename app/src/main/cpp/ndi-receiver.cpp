763ed17c5c733e01f852e3f72db9247304272286
daubli <daniel@daubli.de>
2026-02-07 12:42:17 +0100

    Use the ndi low latency flag when initiating the ndi receiver

--- app/src/main/cpp/ndi-receiver.cpp
+++ app/src/main/cpp/ndi-receiver.cpp
@@ -3,13 +3,22 @@
 #include "ndi-wrapper.h"
 
 extern "C" {
+
     JNIEXPORT jlong JNICALL
-    Java_de_daubli_ndimonitor_ndi_NdiReceiver_receiveCreateDefaultSettings(JNIEnv *env, jclass jClazz) {
-        auto *recv_create_struct = new NDIlib_recv_create_v3_t();
-        auto *ret = NDIlib_recv_create_v3(recv_create_struct);
-        delete recv_create_struct;
-        return reinterpret_cast<jlong>(ret);
-    }
+    Java_de_daubli_ndimonitor_ndi_NdiReceiver_receiveCreateDefaultSettings(
+        JNIEnv *env, jclass jClazz) {
+        NDIlib_recv_create_v3_t recv_create_struct;
+        std::memset(&recv_create_struct, 0, sizeof(recv_create_struct));
+
+        recv_create_struct.color_format = NDIlib_recv_color_format_fastest;
+        recv_create_struct.bandwidth    = NDIlib_recv_bandwidth_highest;
+        recv_create_struct.allow_video_fields = true;
+        recv_create_struct.source_to_connect_to = nullptr;
+
+        NDIlib_recv_instance_t receiver = NDIlib_recv_create_v3(&recv_create_struct);
+
+        return reinterpret_cast<jlong>(receiver);
+}
 
     JNIEXPORT void JNICALL
     Java_de_daubli_ndimonitor_ndi_NdiReceiver_receiveConnect(JNIEnv *env, jclass jClazz, jlong pReceiver, jlong pSource) {
